3db4f16960cb86c6801fced955f02ebd3d4a6aee
Armen Arzumanyan <armenarz@hotmail.com>
2020-06-25 15:17:26 +0400

    Changed exporting process using PHPExcel-1.8 library

--- php/reports/exportSARS.php
+++ php/reports/exportSARS.php
@@ -1,12 +1,8 @@
 <?php
-header("Content-Type: application/xls");
-
 require_once "../connect.php";
 require_once "../authorization.php";
-
-$utf8_bom = chr(239).chr(187).chr(191);
-
-$msg = $utf8_bom;
+require_once "../../../PHPExcel-1.8/Classes/PHPExcel.php";
+require_once "../../../PHPExcel-1.8/Classes/PHPExcel/Writer/Excel2007.php";
 
 if(!isset($_POST["startDate"]) || !isset($_POST["endDate"]))
 {
@@ -51,48 +47,90 @@ else
 
 if($menuId == "SARSLink")
 {
-    $msg.= '
-    <table class="table table-bordered table-hover table-responsive" id="SARSData" border="1">
-        <thead>
-            <tr>
-                <!-- 1. Patients First Name-->
-                <th scope="col">Անուն</th>
-                <!-- 2. Patients Last Name-->
-                <th scope="col">Ազգանուն</th>
-                <!-- 3. Patients Birthdate-->
-                <th scope="col">Ծննդյան&nbsp;ամսաթիվ</th>
-                <!-- 4. Social Security Number-->
-                <th scope="col">ՀԾՀ</th>
-                <!-- 5. Barcode-->
-                <th scope="col">Բարկոդ</th>
-                <!-- 6. Date-->
-                <th scope="col">Ամսաթիվ</th>
-                <!-- 7. Date of Response of the Task-->
-                <th scope="col">Առաջադրանքի պատասխանի ամսաթիվ</th>
-                <!-- 8. Response of Test-->
-                <th scope="col">Թեստավորման պատասխան</th>
-                <!-- 9. Test Date-->
-                <th scope="col">Թեստավորման ամսաթիվ</th>
-                <!-- 10. Sampling Date-->
-                <th scope="col">Նմուշառման ամսաթիվ</th>
-                <!-- 11. Double Testing Difference-->
-                <th scope="col">Կրկնակի թեստավորման տարբերություն</th>
-                <!-- 12. Double Testing-->
-                <th scope="col">Կրկնակի թեստավորում</th>
-                <!-- 13. Directing Institution-->
-                <th scope="col">Ուղեգրող հաստատություն</th>
-                <!-- 14. Registration Address-->
-                <th scope="col">Գրանցման հասցե</th>
-                <!-- 15. Residence Address-->
-                <th scope="col">Բնակության հասցե</th>
-                <!-- 16. OrderId-->
-                <th scope="col">OrderId</th>
-                <!-- 17. Row Number-->
-                <th scope="col">Համար</th>
-            </tr>
-        </thead>
-        <tbody>
-        ';
+    $arr = array(   'Դրական/ positive/ положительный' => 'Դրական', 
+                    'Դրական/positive/положительный' => 'Դրական',
+                    'Բացասական/ negative/ отрицательный' => 'Բացասական',
+                    'Բացասական/ Отрицательный/ Negative' => 'Բացասական',
+                    'Բացասական/negative/отрицательный' => 'Բացասական'
+                    );
+
+    $xls = new PHPExcel();
+
+    $xls->setActiveSheetIndex(0);
+    $sheet = $xls->getActiveSheet();
+    $sheet->setTitle("Sars");
+
+    ///Параметры печати
+    // Формат
+    $sheet->getPageSetup()->SetPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
+    
+    // Ориентация
+    // ORIENTATION_PORTRAIT — книжная
+    // ORIENTATION_LANDSCAPE — альбомная
+    $sheet->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_PORTRAIT);
+    
+    // Поля
+    $sheet->getPageMargins()->setTop(1);
+    $sheet->getPageMargins()->setRight(0.75);
+    $sheet->getPageMargins()->setLeft(0.75);
+    $sheet->getPageMargins()->setBottom(1);
+    
+    // Верхний колонтитул
+    $sheet->getHeaderFooter()->setOddHeader("Название листа");
+    
+    // Нижний колонтитул
+    $sheet->getHeaderFooter()->setOddFooter('&L&B Название листа &R Страница &P из &N');
+
+
+    //title
+    $sheet->setCellValueExplicit("A1", 'Անուն', PHPExcel_Cell_DataType::TYPE_STRING);
+
+    $sheet->setCellValueExplicit("B1", 'Ազգանուն', PHPExcel_Cell_DataType::TYPE_STRING);
+
+    $sheet->setCellValueExplicit("C1", 'Ծննդյան ամսաթիվ', PHPExcel_Cell_DataType::TYPE_STRING);
+
+    $sheet->setCellValueExplicit("D1", 'ՀԾՀ', PHPExcel_Cell_DataType::TYPE_STRING);
+
+    $sheet->setCellValueExplicit("E1", 'Բարկոդ', PHPExcel_Cell_DataType::TYPE_STRING);
+
+    $sheet->setCellValueExplicit("F1", 'Ամսաթիվ', PHPExcel_Cell_DataType::TYPE_STRING);
+
+    $sheet->setCellValueExplicit("G1", 'Առաջադրանքի պատասխանի ամսաթիվ', PHPExcel_Cell_DataType::TYPE_STRING);
+                
+    $sheet->setCellValueExplicit("H1", 'Թեստավորման պատասխան', PHPExcel_Cell_DataType::TYPE_STRING);	
+            
+    $sheet->setCellValueExplicit("I1", 'Թեստավորման ամսաթիվ', PHPExcel_Cell_DataType::TYPE_STRING);
+                    
+    $sheet->setCellValueExplicit("J1", 'Նմուշառման ամսաթիվ', PHPExcel_Cell_DataType::TYPE_STRING);
+                
+    $sheet->setCellValueExplicit("K1", 'Կրկնակի թեստավորման տարբերություն', PHPExcel_Cell_DataType::TYPE_STRING);	
+                    
+    $sheet->setCellValueExplicit("L1", 'Կրկնակի թեստավորում', PHPExcel_Cell_DataType::TYPE_STRING);
+
+    $sheet->setCellValueExplicit("M1", 'Ուղեգրող հաստատություն', PHPExcel_Cell_DataType::TYPE_STRING);
+                        
+    $sheet->setCellValueExplicit("N1", 'Գրանցման հասցե', PHPExcel_Cell_DataType::TYPE_STRING);
+                
+    $sheet->setCellValueExplicit("O1", 'Բնակության հասցե', PHPExcel_Cell_DataType::TYPE_STRING);	
+        
+    $sheet->setCellValueExplicit("P1", 'OrderId', PHPExcel_Cell_DataType::TYPE_STRING);
+                
+    $sheet->setCellValueExplicit("Q1", 'Համար', PHPExcel_Cell_DataType::TYPE_STRING);			
+                
+    //data		
+    /* $sql = "SELECT 
+                pacients.FirstName,
+                pacients.LastName,
+                pacients.birthday,
+                pacients.dopolnitelno,
+                orderresult.OrderId,
+                orderresult.AnalysisResult
+                FROM orderresult
+                INNER JOIN orders ON orders.OrderId=orderresult.OrderId
+                INNER JOIN pacients ON pacients.id=orders.pac_id
+                WHERE orders.OrderDate >= '$startDate' and orders.OrderDate <= '$endDate'
+                    AND orderresult.ReagentId = '1142' and orderresult.double_check = '1'						
+                ORDER BY orderresult.OrderId"; */
     
     $sql = "SELECT 
                 pacients.FirstName,
@@ -130,70 +168,67 @@ if($menuId == "SARSLink")
             INNER JOIN pacients ON orders.pac_id=pacients.id
             WHERE $filter
             ORDER BY t.orderid
-            ";
-    //var_dump($sql);
-    $result = mysqli_query($link, $sql);
-    if($result)
+            ";                
+                
+    $res = mysqli_query($link,$sql);
+    if($res)
     {
-        $i = 0;
-        while($row = mysqli_fetch_array($result))
+        $i = 1;
+        while($row = mysqli_fetch_array($res))
         {
             $i++;
-            $msg.= '
-                <tr>
-                    <!-- 1. Patients First Name-->
-                    <td scope="col">'.$row["FirstName"].'</td>
-                    <!-- 2. Patients Last Name-->
-                    <td scope="col">'.$row["LastName"].'</td>
-                    <!-- 3. Patients Birtddate-->
-                    <td scope="col">'."&nbsp;".$row["birthday"].'</td>
-                    <!-- 4. Social Security Number-->
-                    <td scope="col"></td>
-                    <!-- 5. Barcode-->
-                    <td scope="col"></td>
-                    <!-- 6. Date-->
-                    <td scope="col"></td>
-                    <!-- 7. Date of Response of tde Task-->
-                    <td scope="col"></td>
-                    <!-- 8. Response of Test-->
-                    <td scope="col">';
-                    if($row["AnalysisResult"]=="Դրական/ positive/ положительный")
-                    {
-                        $msg .= "Դրական";
-                    }
-                    elseif($row["AnalysisResult"]=="Բացասական/ negative/ отрицательный")
-                    {
-                        $msg .= "Բացասական";
-                    }
-            $msg.= '</td>
-                    <!-- 9. Test Date-->
-                    <td scope="col"></td>
-                    <!-- 10. Sampling Date-->
-                    <td scope="col"></td>
-                    <!-- 11. Double Testing Difference-->
-                    <td scope="col"></td>
-                    <!-- 12. Double Testing-->
-                    <td scope="col"></td>
-                    <!-- 13. Directing Institution-->
-                    <td scope="col">ՊՐՈՄ-ՏԵՍՏ ՍՊԸ</td>
-                    <!-- 14. Registration Address-->
-                    <td scope="col"></td>
-                    <!-- 15. Residence Address-->
-                    <td scope="col"></td>
-                    <!-- 16. OrderId-->
-                    <td scope="col">'.$row["OrderId"].'</td>
-                    <!-- 17. Row Number-->
-                    <td scope="col">'.$i.'</td>
-                </tr>
-            ';
+            
+            $d1 = $row["birthday"];
+            $date = new DateTime("$d1");
+            
+            $date_format = $date->format('Y-m-d');
+            
+            $sheet->setCellValueExplicit("A".$i, $row["FirstName"], PHPExcel_Cell_DataType::TYPE_STRING);
+            
+            $sheet->setCellValueExplicit("B".$i, $row["LastName"], PHPExcel_Cell_DataType::TYPE_STRING);
+
+            $sheet->setCellValueExplicit("C".$i, $row["birthday"], PHPExcel_Cell_DataType::TYPE_STRING);
+
+            $sheet->setCellValueExplicit("D".$i, $row["dopolnitelno"], PHPExcel_Cell_DataType::TYPE_STRING);
+
+            $sheet->setCellValueExplicit("E".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);
+
+            $sheet->setCellValueExplicit("F".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);
+
+            $sheet->setCellValueExplicit("G".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);
+                        
+            $sheet->setCellValueExplicit("H".$i, $arr[$row["AnalysisResult"]], PHPExcel_Cell_DataType::TYPE_STRING);	
+                    
+            $sheet->setCellValueExplicit("I".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);
+                            
+            $sheet->setCellValueExplicit("J".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);
+                        
+            $sheet->setCellValueExplicit("K".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);	
+                    
+            $sheet->setCellValueExplicit("L".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);
+                            
+            $sheet->setCellValueExplicit("M".$i, "ՊՐՈՄ-ՏԷՍՏ ՍՊԸ", PHPExcel_Cell_DataType::TYPE_STRING);
+
+            $sheet->setCellValueExplicit("N".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);
+                                
+            $sheet->setCellValueExplicit("O".$i, "", PHPExcel_Cell_DataType::TYPE_STRING);
+                        
+            $sheet->setCellValueExplicit("P".$i, $row["OrderId"], PHPExcel_Cell_DataType::TYPE_STRING);
+                        
+            $sheet->setCellValueExplicit("Q".$i, ($i-1), PHPExcel_Cell_DataType::TYPE_STRING);				
+            
         }
     }
-    
-    $msg.= '
-        </tbody>
-    </table>
-    ';
 }
-header("Content-Disposition: attachement; filename=sars.xls");
-echo $msg;
+
+header("Expires: Mon, 1 Apr 1974 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Content-type: application/vnd.ms-excel");
+header("Content-Disposition: attachment; filename=sars.xls");
+	
+$objWriter = new PHPExcel_Writer_Excel5($xls);
+$objWriter->save('php://output'); 	
+
 ?>
