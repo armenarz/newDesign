c0e76011cd7dc69910ad2c401dd3f1e5cbcbebf5
Armen Arzumanyan <armenarz@hotmail.com>
2021-02-17 14:20:11 +0400

    fixed only armenian functionality

--- php/reports/extractArmenianName.php
+++ php/reports/extractArmenianName.php
@@ -1,41 +1,70 @@
-<?php
+<?php 
+//header('Content-Type: text/html; charset=utf-8');
+//$str = "Tudorica- Cristinel Տուդորիկա- Քրիստինել Tudorica- Cristinel";
+//$str = "Tudorica- Cristinel Տուդորիկա- Քրիստինել Тудорика- Кристинел";
+//$str = "Tudorica- Cristinel ";
+//$str = "---- Տուդորիկա- Քրիստինել";
+//echo $str."<br>";
+//echo "extarctArmenianName(str)=".extarctArmenianName($str)."<br>";
+
+
 function extarctArmenianName($name)
 {
-    // check if source string includes "-"character
-    if(strpos($name,"-"))
-    {
-        // cleaning whitespace next to "-" character
-        $temp = explode("-",$name);
-        if(count($temp)>0)
-        {
-            $name = "";
-            for($tempIndex = 0; $tempIndex < count($temp); $tempIndex++)
-            {
-                if($tempIndex > 0)
-                {
-                    $name .= trim($temp[$tempIndex]);
-                }
-                else
-                {
-                    $name .= "-".trim($temp[$tempIndex]);
-                }
-            }
-        }
-    }
+    // replacing 3 whitespaces with one
+    $name = str_replace("   "," ", $name);
+    
+    // replacing 2 whitespaces with one
+    $name = str_replace("  "," ", $name);
+    
+    // cleaning whitespaces around the dashes
+    $name = str_replace(" -" , "-" , $name);
+
+    $name = str_replace("- " , "-" , $name);
+
     // separating by whitespace
     $temp = explode(" ",$name);
-    if(count($temp) == 3)
+   
+    $name_am = "";
+    $name_en = "";
+    $name_ru = "";
+   
+    for($i = 0; $i < count($temp); $i++)
     {
-        return $temp[0];
+        $temp[$i] = trim($temp[$i]);
+        // if text is armenian
+        if(preg_match('/[Ա-Ֆա-ֆ]/u', $temp[$i]))
+        {
+            $name_am .= $temp[$i]." ";
+        }
+        // if text is english
+        if(preg_match('/[A-Za-z]/u', $temp[$i]))
+        {
+            $name_en .= $temp[$i]." ";
+        }
+        // if text is russian
+        if(preg_match('/[А-Яа-яЁё]/u', $temp[$i]))
+        {
+            $name_ru .= $temp[$i]." ";
+        }
     }
-    elseif(count($temp) == 6)
+
+    $name_am = trim($name_am);
+    $name_en = trim($name_en);
+    $name_ru = trim($name_ru);
+    if($name_am != "")
     {
-        return $temp[0]." ".$temp[1];
+        $name = $name_am;
+    }
+    elseif($name_en != "")
+    {
+        $name = $name_en;
     }
     else
     {
-        return $name;
+        $name = $name_ru;
     }
+    
+    return $name;
 }
 
 ?>
